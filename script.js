02cd577199a621f3478a3b21b27db949c1ff68db
Bone81-Tech <yusufdaeng46@gmail.com>
2025-08-04 22:57:20 +0700

    fix: Update API_URL to new endpoint with server-side validation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 
 // --- KONFIGURASI ---
-const API_URL = 'https://script.google.com/macros/s/AKfycbxGfEtqcfV4C7FX_BHMiHu9EsvrfsgDXpV4C7FX_BHMiHu9EsvrfsgDXpV4mSt5DuYLmBAabijN16ZkjiWiDnlszp8/exec'; 
+const API_URL = 'https://script.google.com/macros/s/AKfycbx_G8GZSpQNhLJtFooHRvey7CGv2f7VZ7xZeg3CueCyjTDk4GlJm2nvykZu0Q7DUg/exec'; 
 // !!! GANTI DENGAN URL API NEWSLETTER ANDA SETELAH DI-DEPLOY !!!
 const NEWSLETTER_API_URL = 'https://script.google.com/macros/s/AKfycbyyfVSQSOWb1eYURJig5B9cw4g1v5eTbdcULQmFOlwBkGtzcbzTM9m_W0sAktpOK3M/exec'; 
 
